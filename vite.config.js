17fceb7c82111c9d1d31843080a5b2cdef99b8ee
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2024-10-04 18:51:38 -0700

    correct build folder

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr' 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
-    svgr({ 
+    react(),
+    svgr({
       svgrOptions: {
-        include: '**/*.svg'
+        include: "**/*.svg",
       },
     }),
-  ], 
-})
+  ],
+  build: {
+    outDir: "./build",
+    emptyOutDir: true, // also necessary
+  },
+});
