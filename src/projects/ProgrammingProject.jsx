b43322bc1b82f7ea4ccb8307f286269df706fc67
Nigel <63932931+Whackalenso@users.noreply.github.com>
2024-11-04 19:41:13 +0000

    adsf

--- src/projects/ProgrammingProject.jsx
+++ src/projects/ProgrammingProject.jsx
@@ -3,6 +3,9 @@ export default function ProgrammingProject() {
         <h2>Arduino Programming</h2>
         <p className="subtitle">Updated: 10/29/24</p>
         <div className="project-body">
+            <h3>Programming Concepts</h3>
+            <p><b style={{fontSize: "14pt"}}>Variables</b> store data. Variables are declared in the form <code>&lt;type&gt; &lt;name&gt; = &lt;value&gt;;</code> (e.g. <code>int pin = 2;</code>). The basic data types used in these Arduino exercises are integers, or whole numbers, and booleans, which are either <code>true</code> or <code>false</code>. In code, these are represented by <code>int</code> and <code>boolean</code>, respectively. You can reference the value of a variable by just writing the name of the variable. To set a variable to a new value, you can use <code>&lt;variableName&gt; = &lt;newValue&gt;;</code> (e.g. <code>pin = 3;</code>).</p>
+            <p><b style={{fontSize: "14pt"}}>Arrays</b> are types of variables that store a list of data. Arrays are declared in the form <br/><code>&lt;type&gt; &lt;name&gt;[&lt;numOfItems&gt;] = &#123;&lt;each item, comma separated&gt;&#125;;</code> (e.g. <code>int pins[3] = &#123;2, 3, 4&#125;;</code>). You can reference the value of an item in the array at a certain position by writing the name of the array and then the index of which item you want. For example, <code>pins[2]</code> will reference the value of the 3rd item in the array (because indeces start at 0), which is <code>4</code>.</p>
             <h3>Exercise 19</h3>
             <div className="columns" style={{gap: "60px"}}>
                 <div>
@@ -21,6 +24,14 @@ export default function ProgrammingProject() {
             </div>
             <p><i>Exercise 39 uploaded to the Arduino:</i></p>
             <img src="./ex_39.gif" style={{height: "500px"}}/>
+            <h3>Exercise 49</h3>
+            <div className="columns" style={{gap: "60px", marginBottom: "50px"}}>
+                <div>
+                    <img src="./ex_49_dir.png" style={{width: "320px"}}/>
+                    <p style={{paddingTop: "20px"}}>hi this is the expalnation</p>
+                </div>
+                <img className="code" src="./ex_49.png" />
+            </div>
         </div>
     </div>
 }
